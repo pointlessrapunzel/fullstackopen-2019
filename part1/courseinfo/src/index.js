63079906d83d3fd44fc819b8de9c700c5c0e8061
Vlad <45142318+pointlessrapunzel@users.noreply.github.com>
2019-12-15 20:05:32 +0500

    fix typos in Total component

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -27,7 +27,7 @@ const Total = (props) => {
   return (
     <div>
       <p>
-        Number of exercises {props.course.parts[0].exercises + props.course.parts[0].exercises + props.course.parts[0].exercises}
+        Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}
       </p>
     </div>
   )
