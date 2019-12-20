b97de4ed70a015b348adde945c42c51c8a53136b
pointlessrapunzel <vladace9@gmail.com>
2019-12-20 06:12:43 +0500

    ex 2.8, implement adding a number, refactor using html tables

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,27 +2,45 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    {name: 'Arto Hellas'}
+    {
+      name: 'Arto Hellas',
+      number: '040-1234567'
+    }
   ])
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
-  const listNames = () => (
-    persons.map(person => <li key={person.name.trim()}>{person.name}</li>)
+  // render the list of numbers
+  const numberRows = () => (
+    persons.map(person =>
+      <tr key={person.name.trim()}>
+        <td>{person.name}</td>
+        <td>{person.number}</td>
+      </tr>
+    )
   )
 
-  const addPerson = (event) => {
+  // add a new name to the list
+  const addNumber = (event) => {
     event.preventDefault()
     const personObj = {
       name: newName.trim(),
+      number: newNumber.trim()
     }
 
-    // without .trim and lowercase, the check would fail
+    // disallow adding an already existing name
+    // .trim and lowercase are needed for successful check
     if (persons.find(person => 
           person.name.toLowerCase() === newName.trim().toLowerCase())) {
       alert(`${newName} is already added to phonebook`)
+    } 
+    // if name or number fields are empty
+    else if (!newName || !newNumber) {
+      alert("You have to enter a name and a number!")
     } else {
       setPersons(persons.concat(personObj))
       setNewName('')
+      setNewNumber('')
     }
   }
 
@@ -30,11 +48,14 @@ const App = () => {
     setNewName(event.target.value) 
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
+      <form onSubmit={addNumber}>
         <div>
           name: <input 
                   value={newName}
@@ -42,14 +63,28 @@ const App = () => {
                 />
         </div>
         <div>
+          number: <input
+                    value={newNumber}
+                    onChange={handleNumberChange}
+                  />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
-        <ul>
-          {listNames()}
-        </ul>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Number</th>
+            </tr>
+          </thead>
+          <tbody>
+            {numberRows()}
+          </tbody>
+        </table>
       </div>
     </div>
   )
