78aa0fe3033a428588f59a370879b8665d34c084
pointlessrapunzel <vladace9@gmail.com>
2020-01-12 21:51:01 +0500

    finish ex4.4, add a totalLikes test

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = blogs => {
   return 1
 }
 
+const totalLikes = blogs => {
+  return blogs.reduce((total, blog) => total + blog.likes, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
