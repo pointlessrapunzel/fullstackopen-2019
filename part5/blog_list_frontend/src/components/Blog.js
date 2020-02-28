f2a8465f2e742e1c0f138aea0d726d83c4b007b2
pointlessrapunzel <vladace9@gmail.com>
2020-02-28 13:57:41 +0500

    make blogs expandable

--- part5/blog_list_frontend/src/components/Blog.js
+++ part5/blog_list_frontend/src/components/Blog.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
+  const [shown, setShown] = useState(false)
+
+  const handleShown = () => setShown(!shown)
+
+  const user = blog.user ? blog.user.username : 'added with React'
+
+  if (!shown) {
+    return (
+      <div className='blog'>
+        <span className='blog-title' onClick={handleShown}>
+          {blog.title} {blog.author}
+        </span>
+      </div>
+    )
+  } else {
+    return (
+      <div className='blog'>
+        <span className='blog-title' onClick={handleShown}>
+          {blog.title} {blog.author}
+        </span>
+        <div>{blog.url}</div>
+        <div>
+          {blog.likes} likes
+          <button type='button'>like</button>
+        </div>
+        <div>added by {user}</div>
+      </div>
+    )
+  }
+}
 
 export default Blog
